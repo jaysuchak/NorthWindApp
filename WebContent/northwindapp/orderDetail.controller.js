f4a5bcd0d914ca1eec56341a6525b57085456630
JD <jaysuchak@gmail.com>
2016-04-22 10:55:08 +0530

    retretrrtretr

--- WebContent/northwindapp/orderDetail.controller.js
+++ WebContent/northwindapp/orderDetail.controller.js
@@ -10,76 +10,64 @@ sap.ui.define([
  
 	var Controller = Controller.extend("northwind.northwindapp.orderDetail", {
 		_oDialog: null,
-
-
-
-
-					/**
-					 * Called when a controller is instantiated and its View
-					 * controls (if available) are already created. Can be used
-					 * to modify the View before it is displayed, to bind event
-					 * handlers and do other one-time initialization.
-					 * 
-					 * @memberOf northwindapp.productDetail
-					 */
-					onInit : function() {
-						var productId;
-						var productName;
-						var that = this;
-						sap.ui.core.UIComponent.getRouterFor(this).attachRouteMatched(
-										function(oEvent) {
-											productId = oEvent.getParameter("arguments").orderProductId;
-											productName = oEvent.getParameter("arguments").orderProductName;
-											var sName = oEvent.getParameter("name");
-											if (sName === "orderDetail") {
-												that.getView().byId("idOrdeDetail").setTitle("Order Deatils of "+ productName);
-												var url = "http://services.odata.org/V4/Northwind/Northwind.svc/Products("+productId+")/Order_Details";
-												var JSONModel = new sap.ui.model.json.JSONModel();											
-												that.getView().byId("idOrderDetailTable").setModel(JSONModel);
-												jQuery.ajax({
-															url : url,
-															method : "GET",
-															async : false,
-															dataType : "json",
-															success : function(data) {
-																JSONModel.setData(data);															         
-															},
-															error : function() {
-																alert('Error in json call');
-															}
-												});
-											}
-										});
-					},
-					handleLinkPress: function (oEvent) {
-						var bReplace = jQuery.device.is.phone ? false : true;
-						sap.ui.core.UIComponent.getRouterFor(this).navTo(
-								"order", {
-									orderProductId:productId,
-									orderProductName:productName,
-									orderId:oEvent.getSource().getText()
-								}, bReplace);
-					},
-					getRouter : function() {
-						return sap.ui.core.UIComponent.getRouterFor(this);
-					},
-					handleChange: function (oEvent) {
-						var sFrom = oEvent.getParameter("from");
-						var sTo = oEvent.getParameter("to");
-						var bValid = oEvent.getParameter("valid");
-			 
-						this._iEvent++;
-			 
-						var oText = this.byId("TextEvent");
-						oText.setText("Id: " + oEvent.oSource.getId() + "\nFrom: " + sFrom + "\nTo: " + sTo);
-			 
-						var oDRS = oEvent.oSource;
-						if (bValid) {
-							oDRS.setValueState(sap.ui.core.ValueState.None);
-						} else {
-							oDRS.setValueState(sap.ui.core.ValueState.Error);
-						}
-					}
+		onInit : function() {
+			var productId;
+			var productName;
+			var that = this;
+			sap.ui.core.UIComponent.getRouterFor(this).attachRouteMatched(
+							function(oEvent) {
+								productId = oEvent.getParameter("arguments").orderProductId;
+								productName = oEvent.getParameter("arguments").orderProductName;
+								var sName = oEvent.getParameter("name");
+								if (sName === "orderDetail") {
+									that.getView().byId("idOrdeDetail").setTitle("Order Deatils of "+ productName);
+									var url = "http://services.odata.org/V4/Northwind/Northwind.svc/Products("+productId+")/Order_Details";
+									var JSONModel = new sap.ui.model.json.JSONModel();											
+									that.getView().byId("idOrderDetailTable").setModel(JSONModel);
+									jQuery.ajax({
+												url : url,
+												method : "GET",
+												async : false,
+												dataType : "json",
+												success : function(data) {
+													JSONModel.setData(data);															         
+												},
+												error : function() {
+													alert('Error in json call');
+												}
+									});
+								}
+							});
+		},
+		handleLinkPress: function (oEvent) {
+			var bReplace = jQuery.device.is.phone ? false : true;
+			sap.ui.core.UIComponent.getRouterFor(this).navTo(
+					"order", {
+						orderProductId:productId,
+						orderProductName:productName,
+						orderId:oEvent.getSource().getText()
+					}, bReplace);
+		},
+		getRouter : function() {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		handleChange: function (oEvent) {
+			var sFrom = oEvent.getParameter("from");
+			var sTo = oEvent.getParameter("to");
+			var bValid = oEvent.getParameter("valid");
+ 
+			this._iEvent++;
+ 
+			var oText = this.byId("TextEvent");
+			oText.setText("Id: " + oEvent.oSource.getId() + "\nFrom: " + sFrom + "\nTo: " + sTo);
+ 
+			var oDRS = oEvent.oSource;
+			if (bValid) {
+				oDRS.setValueState(sap.ui.core.ValueState.None);
+			} else {
+				oDRS.setValueState(sap.ui.core.ValueState.Error);
+			}
+		}
 	});
 	return Controller;
 	 
